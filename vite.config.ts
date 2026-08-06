15418224ea71ca65d7accd5629eb5860c24227c9
veer <veernagda2006@gmail.com>
2026-08-07 02:12:43 +0530

    fix: clean build with relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/food-on-the-move/",
+  base: "./",
   build: {
     outDir: "docs",
   },
